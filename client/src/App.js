53ee411dace48af2fc53029448f349e99eedccce
YONGSU JEONG <53428342+jos50275266@users.noreply.github.com>
2020-07-18 23:30:33 +0900

    modification

--- client/src/App.js
+++ client/src/App.js
@@ -8,9 +8,6 @@ function App() {
   return (
     <Router>
       <div className="App">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
         <a
           className="App-link"
           href="https://reactjs.org"
